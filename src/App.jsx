7faa03d140bd1204da1bbfdee3912ec7189a5962
Mayank Kumar Singh <mksingh94520@gmail.com>
2024-08-21 14:39:33 +0530

    assessment is completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CollegeList from './components/CollegeList';
+// import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sportsdunia</h1>
+      <CollegeList />
+    </div>
+  );
+}
+
+export default App;
